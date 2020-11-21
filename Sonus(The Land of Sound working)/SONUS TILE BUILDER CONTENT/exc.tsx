8d3ff263d930dd5f2e3beb64cb326bc91561abcd
Dylan Kapustka <41035746+Kapoot@users.noreply.github.com>
2020-11-21 10:41:24 -0600

    Delete exc.tsx

--- Sonus(The Land of Sound working)/SONUS TILE BUILDER CONTENT/exc.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="exc" tilewidth="32" tileheight="32" tilecount="88" columns="11">
- <image source="../Downloads/Extra_Unfinished4.png" width="360" height="266"/>
-</tileset>
