6486621ed930688ff04216de5941eb27fc3b4458
Dylan Kapustka <41035746+Kapoot@users.noreply.github.com>
2020-11-21 10:40:16 -0600

    Delete transparent-bg-tiles.tsx

--- Sonus(The Land of Sound working)/SONUS TILE BUILDER CONTENT/transparent-bg-tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="transparent-bg-tiles" tilewidth="32" tileheight="32" tilecount="99" columns="11">
- <image source="town_rpg_pack/graphics/transparent-bg-tiles.png" width="352" height="288"/>
-</tileset>
